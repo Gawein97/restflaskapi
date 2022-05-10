092903e09f176098b4506711680a7e8fd9e65442
Artem Koros <arkoros97@gmail.com>
2022-05-11 00:57:06 +0300

    user register check and savepoint

--- libraryapi/resources/user_register.py
+++ libraryapi/resources/user_register.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask_restful import Resource, reqparse
+from libraryapi.models import User
 
 
 class UserRegister(Resource):
@@ -9,6 +10,8 @@ class UserRegister(Resource):
 
     def post(self):
         args = self.parser.parse_args(strict=True)
+        if User.find_by_username(args["username"]):
+            return {"message": "user already exists"}, 400
         with sqlite3.connect("data.db") as conn:
             cursor = conn.cursor()
             query = "INSERT INTO users VALUES (NULL, ?, ?)"
