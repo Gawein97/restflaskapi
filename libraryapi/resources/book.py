ad577cfce8a6aeda6adb2d1ec8ab065106b39b8d
Artem Koros <arkoros97@gmail.com>
2022-05-11 04:58:55 +0300

    connect db in book resources

--- libraryapi/resources/book.py
+++ libraryapi/resources/book.py
@@ -16,11 +16,7 @@ class BookApi(Resource):
     parser.add_argument("price", required=True, type=float)
 
     def get(self, name: str):
-        with sqlite3.connect("data.db") as conn:
-            cursor = conn.cursor()
-            query = "SELECT * FROM books WHERE name=?"
-            result = cursor.execute(query, (name,))
-            row = result.fetchone()
+        row = self.find_by_name(name)
         if row:
             try:
                 book = Book(*row)
@@ -29,35 +25,83 @@ class BookApi(Resource):
             return BookSerializer(book).data, 200
         return {"message": f"no book with name {name}"}, 404
 
+    @classmethod
+    def find_by_name(cls, name):
+        with sqlite3.connect("data.db") as conn:
+            cursor = conn.cursor()
+            query = "SELECT * FROM books WHERE name=?"
+            result = cursor.execute(query, (name,))
+            row = result.fetchone()
+        return row
+
+    @classmethod
+    def insert(cls, book):
+        with sqlite3.connect("data.db") as conn:
+            cursor = conn.cursor()
+            query = "INSERT INTO books VALUES (?, ?, ?, ?)"
+            cursor.execute(query, (book.name, book.author, book.pages, book.price))
+            conn.commit()
+
     @jwt_required()
     def post(self):
         args = self.parser.parse_args(strict=True)
-        if next(filter(lambda b: b.name == args["name"], books), None) is not None:
+        if self.find_by_name(args["name"]):
             return {'message': "the book with name '{}' already exists.".format(args["name"])}, 400
         new_book = Book(args["name"], args["author"], args["pages"], args["price"])
-        books.append(new_book)
+        try:
+            self.insert(new_book)
+        except:
+            return {"message": "error occurred"}, 500
         return BookSerializer(new_book).data, 201
 
     @jwt_required()
     def delete(self, name):
         # todo: implement check if book was actually deleted now it shows every time
-        global books
-        books = list(filter(lambda b: b.name != name, books))
+        row = self.find_by_name(name)
+        if not row:
+            return {"message": "book doesn't exist"}, 400
+        with sqlite3.connect("data.db") as conn:
+            cursor = conn.cursor()
+            query = "DELETE FROM books WHERE name=?"
+            cursor.execute(query, (name,))
+            conn.commit()
         return {"message": "book deleted"}, 200
 
     @jwt_required()
     def put(self):
         args = self.parser.parse_args(strict=True)
-        book = next(filter(lambda b: b.name == args["name"], books), None)
-        if book is None:
-            book = Book(args["name"], args["author"], args["pages"], args["price"])
-            books.append(book)
+        row = self.find_by_name(args["name"])
+        book = Book(args["name"], args["author"], args["pages"], args["price"])
+        if not row:
+            try:
+                self.insert(book)
+                return BookSerializer(book).data, 201
+            except:
+                return {"message": "error occurred"}, 500
         else:
-            book.update(**args)
+            with sqlite3.connect("data.db") as conn:
+                cursor = conn.cursor()
+                query = """
+                UPDATE books
+                SET name=?,
+                    author=?,
+                    pages=?,
+                    price=?
+                WHERE name=?
+                """
+                cursor.execute(query, (book.name, book.author, book.pages, book.price, book.name))
+                conn.commit()
         return BookSerializer(book).data, 200
 
 
 class BooksListApi(Resource):
     """api resource for /books endpoint, lists all books"""
     def get(self):
+        with sqlite3.connect("data.db") as conn:
+            cursor = conn.cursor()
+            query = "SELECT * FROM books"
+            result = cursor.execute(query)
+            # This looks like a pile of garbage should change serializer to Marshall
+            # TODO: Change serializer to Marshall or refactor code idk
+            books = [Book(*b) for b in result.fetchall()]
         return {"books": BookSerializer(books, many=True).data}, 200
